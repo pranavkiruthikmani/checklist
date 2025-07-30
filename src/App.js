91b2f3dc6fc5289df53d97ddd4ce3307913a1072
Pranav Kiruthik <pranavkiruthikmani@gmail.com>
2025-07-30 07:03:45 +0000

    Now saves to local storage

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@ function App() {
 
   const [checkbox, setCheckbox] = useState([]);
   const [textfield, setTextfield] = useState('');
+  const [started, setStarted] = useState(false);
+
+  useEffect(() => {
+  const localvalues = JSON.parse(localStorage.getItem('checkbox'));
+  if (localvalues) {
+    setCheckbox(localvalues);
+    setStarted(true);
+  }
+  }, [])
 
   const updateArray = () => {
-    if (textfield !== "") {
-      setCheckbox([...checkbox, {textfield}]);
-      setTextfield('');
+    if (textfield.trim() !== "") {
+      const entry = {
+        id: Date.now(),
+        text: textfield.trim()
+      }
+      setCheckbox([...checkbox, entry])
+      setTextfield('')
+    }     
+  }
+
+  const clearArray = () => {
+    if (checkbox.length > 0) {
+      setCheckbox([])
+      localStorage.clear();
     }
-     
   }
 
   useEffect(() => {
-    console.log(checkbox);
-  }, [checkbox])
+    if (started === true) {
+      localStorage.setItem('checkbox', JSON.stringify(checkbox));
+    }    
+    console.log(JSON.parse(localStorage.getItem('checkbox')));
+  }, [checkbox, started])
 
   return (
     <div>
@@ -25,14 +47,17 @@ function App() {
         <button onClick={updateArray} className='button-30'>
           Add Reminder
         </button>
+        <button onClick={clearArray} className='button-30'>
+          Clear
+        </button>
       </div> 
         <div className='valuelist'>
           {checkbox.map((current, index) => {
-            const key = current.textfield - index;
+            const key = current.id;
             return(
               <div key={key}>
-                <input className='checkbox' type='checkbox' id='index' name='index' value={current}/>
-                <label htmlFor='index' className='checkboxlabel'>{current.textfield}</label>
+                <input className='checkbox' type='checkbox' id={key} name={key} value={current.text}/>
+                <label htmlFor={key} className='checkboxlabel'>{current.text}</label>
               </div>
             )})}
         </div>
